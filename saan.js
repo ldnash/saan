1b4044eae77e9042f5ca7782fb0502f04ac423bc
Logan Nash <nash.logan@gmail.com>
2014-09-29 12:17:17 -0400

    Geolocation button and navbar

--- saan.js
+++ saan.js
@@ -7,7 +7,18 @@
 		div: 'map_canvas',
 		center: L.latLng(29.369245, -98.475088),
 		zoom: 13,
+		locateControl: true,
+		homeControl: true,
+		//legendControl: {
+			//html: 'test',
+			//position: 'bottomright'
+			//}
 		});
+	
+	//Resize map to accommodate navbar
+	var marginHeight = $('#saannav').height()
+	console.log(marginHeight);
+	$('#map_canvas').css('margin-top', marginHeight);
 	
 	}
 	
