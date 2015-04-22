c6aa669f32b8621795aa91fe12b3ac54e1ffa411
Logan Nash <nash.logan@gmail.com>
2015-04-22 11:23:17 -0400

    Legend changes

--- embed.js
+++ embed.js
@@ -8,6 +8,7 @@ function legend(){
 		'<h3>Legend</h3>' +
 		'<ul id = "legendList" class = "list-group">' +
 		  '<li><div class="legendRow"><div class="legendIcon"><img src="icons/riverRoute.gif" alt="River Walk"/></div>River Walk Trail (Hike + Bike)</a></div></li>' +
+		  '<li><div class="legendRow"><div class="legendIcon"><img src="icons/riverRoute_ped.gif" alt="River Walk (Hike only)"/></div>River Walk Trail (Hike Only)</a></div></li>' +
 		  '<li><div class="legendRow"><div class="legendIcon"><img src="icons/onRoadRoutes_bikePed.gif" alt="Suggested Walk + Bike Streets"/></div>Road Routes (Hike + Bike + Drive)</a></div></li>' +
 		  '<li><div class="legendRow"><div class="legendIcon"><img src="icons/onRoadRoutes_bikeonly.gif" alt="Bike Only Streets for Experienced Cyclists"/></div>Road Routes (Bike + Drive)</a></div></li>' +
 		  '<li><div class="legendRow"><div class="legendRow"><div class="legendIcon"><img src="icons/aceRoute.gif" alt="Acequia Trails (Unpaved)"/></div>Acequia Trails (Unpaved)</a></div></li>' +
