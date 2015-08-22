96577baeb5415162a3438d5045a0add618b46b55
Nate Irwin <nate@nateirwin.net>
2015-08-22 10:13:53 -0600

    Final modifications

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # San Antonio Missions Trip Planner
 
-Alternative transportation trip planner for the San Antonio Missions National Historical Park. Uses [NPMap.js](https://github.com/nationalparkservice/npmap.js/>npmap.js).
+Alternative transportation trip planner for the San Antonio Missions National Historical Park. Uses [NPMap.js](https://github.com/nationalparkservice/npmap.js/).
+
+## Data sources
+
+- NPS staff
+- B-cycle
+- [AVAIL Labs GTFS API](http://api.availabs.org/gtfs/)
+  - VIA metro
+
+## Tools
+
+- Bootstrap
+- Leaflet
+- NPMap.js
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,14 @@
 /* globals $, L, legend */
 
-var $legend = $('#trueLegendContent')
-var $legendButton = $('#trueLegendButton')
-var majorLayers = {}
-var minorLayers = {}
-var topLayers = {}
-var bcycleData
-var map
-var transitRoutes
-var transitStops
+var $legend = $('#trueLegendContent');
+var $legendButton = $('#trueLegendButton');
+var majorLayers = {};
+var minorLayers = {};
+var topLayers = {};
+var bcycleData;
+var map;
+var transitRoutes;
+var transitStops;
 
 function initialize () {
   map = L.npmap.map({
@@ -19,23 +19,23 @@ function initialize () {
     div: 'map-canvas',
     events: [{
       fn: function () {
-        var zoom = map.getZoom()
+        var zoom = map.getZoom();
 
         if (zoom >= 15) {
           for (var i in minorLayers) {
-            minorLayers[i].addTo(map)
+            minorLayers[i].addTo(map);
           }
 
           for (var k in topLayers) {
-            map.removeLayer(topLayers[k])
+            map.removeLayer(topLayers[k]);
           }
         } else {
           for (var j in minorLayers) {
-            map.removeLayer(minorLayers[j])
+            map.removeLayer(minorLayers[j]);
           }
 
           for (var l in topLayers) {
-            topLayers[l].addTo(map)
+            topLayers[l].addTo(map);
           }
         }
       },
@@ -43,11 +43,11 @@ function initialize () {
     }],
     locateControl: true,
     zoom: 13
-  })
-  $('#map-canvas').css('padding-top', $('.navbar').height())
+  });
+  $('#map-canvas').css('padding-top', $('.navbar').height());
 
   try {
-    legend()
+    legend();
   } catch(e) {}
 
   majorLayers.aceLines = L.npmap.layer.geojson({
@@ -63,7 +63,7 @@ function initialize () {
     },
     tooltip: 'Acequia Trails',
     url: 'data/acequias_trails.geojson'
-  }).addTo(map)
+  }).addTo(map);
   majorLayers.aceSites = L.npmap.layer.geojson({
     popup: {
       description: '{{Info}}',
@@ -77,10 +77,10 @@ function initialize () {
     },
     tooltip: '{{Name}}',
     url: 'data/acequias.geojson'
-  }).addTo(map)
+  }).addTo(map);
   majorLayers.missions = L.npmap.layer.geojson({
     popup: function (feature) {
-      return '<b>' + feature.Full_Name + '</b><p>' + feature.Desc + '</p><p><a href=' + feature.Link + '>More information on nps.gov</a></p>' + '<p><a href=' + feature.Directions + '>Directions</a></p>'
+      return '<b>' + feature.Full_Name + '</b><p>' + feature.Desc + '</p><p><a href=' + feature.Link + '>More information on nps.gov</a></p>' + '<p><a href=' + feature.Directions + '>Directions</a></p>';
     },
     styles: function (feature) {
       return {
@@ -88,18 +88,18 @@ function initialize () {
           iconSize: [50, 50],
           iconUrl: feature.iconUrl
         }
-      }
+      };
     },
     tooltip: 'Mission {{Name}}',
     url: 'data/cmpndsites.geojson'
-  }).addTo(map)
+  }).addTo(map);
   majorLayers.missionTrails = L.npmap.layer.geojson({
     color: '#78591f',
     dashArray: '5, 10',
     opacity: 0.8,
     tooltip: 'Mission Trail',
     url: 'data/missiontrails_dry.geojson'
-  })
+  });
   majorLayers.trailsNew = L.npmap.layer.geojson({
     popup: {
       description: 'The River Walk and connecting trails.',
@@ -113,7 +113,7 @@ function initialize () {
     },
     tooltip: 'River Walk',
     url: 'data/mainTrails.geojson'
-  }).addTo(map)
+  }).addTo(map);
   minorLayers.minor = L.npmap.layer.geojson({
     popup: {
       title: '{{Facility}}',
@@ -121,7 +121,7 @@ function initialize () {
     },
     tooltip: '{{Facility}}',
     url: 'data/CombinedFacilities.geojson'
-  })
+  });
   minorLayers.onStreetBikeOnly = L.npmap.layer.geojson({
     color: '#ff9900',
     dashArray: '5, 10',
@@ -131,7 +131,7 @@ function initialize () {
     },
     tooltip: 'Bike-only Road Routes',
     url: 'data/BikeOnly_onRoad.geojson'
-  })
+  });
   minorLayers.onStreetBikePed = L.npmap.layer.geojson({
     popup: {
       description: 'For cyclists and pedestrians.',
@@ -145,7 +145,7 @@ function initialize () {
     },
     tooltip: 'Road Routes',
     url: 'data/BikePed_onRoad.geojson'
-  })
+  });
   minorLayers.parking = L.npmap.layer.geojson({
     popup: {
       title: '{{Parking}}'
@@ -157,7 +157,7 @@ function initialize () {
     },
     tooltip: 'Parking Area',
     url: 'data/parking.geojson'
-  })
+  });
   minorLayers.ped = L.npmap.layer.geojson({
     popup: {
       description: 'Only pedestrians are allowed on downtown sections of the River Walk and certain other trails.',
@@ -172,7 +172,7 @@ function initialize () {
     },
     tooltip: 'Pedestrians-only trail',
     url: 'data/pedTrails.geojson'
-  })
+  });
   minorLayers.secondary = L.npmap.layer.geojson({
     color: '#ff0044',
     opacity: 0.8,
@@ -182,7 +182,7 @@ function initialize () {
     },
     tooltip: 'River Walk',
     url: 'data/secondaryTrails.geojson'
-  })
+  });
   minorLayers.visitorCenters = L.npmap.layer.geojson({
     popup: {
       title: '{{Facility}}'
@@ -194,7 +194,7 @@ function initialize () {
     },
     tooltip: '{{Facility}}',
     url: 'data/visitorCenters.geojson'
-  })
+  });
   topLayers.minor = L.npmap.layer.geojson({
     cluster: true,
     popup: {
@@ -203,11 +203,14 @@ function initialize () {
     },
     tooltip: '{{Facility}}',
     url: 'data/CombinedFacilities.geojson'
-  }).addTo(map)
+  }).addTo(map);
   transitRoutes = L.npmap.layer.geojson({
-    popup: function (feature) {
-      return '<b>VIA Bus Route 42</b><p><a href=http://www.viainfo.net/BusService/RiderTool.aspx?ToolChoice=Schedules>Check VIA Transit for service times and schedules</a></p>'
-    },
+    popup: '' +
+      '<b>VIA Bus Route 42</b>' +
+      '<p>' +
+        '<a href=http://www.viainfo.net/BusService/RiderTool.aspx?ToolChoice=Schedules>Check VIA Transit for service times and schedules</a>' +
+      '</p>' +
+    '',
     styles: {
       line: {
         'stroke': '#45000d',
@@ -215,7 +218,7 @@ function initialize () {
       }
     },
     url: 'data/viamissions.geojson'
-  })
+  });
   transitStops = L.npmap.layer.geojson({
     popup: {
       description: 'Schedules vary significantly. <br> <a href=http://www.viainfo.net/BusService/RiderTool.aspx?ToolChoice=Schedules>Check VIA Transit for service times and schedules</a>',
@@ -228,75 +231,77 @@ function initialize () {
     },
     tooltip: 'Bus Stop',
     url: 'data/cutviastops.geojson'
-  })
+  });
 }
 
 $(document).ready(function () {
-  initialize()
+  var apiKey = '49AB876F-017E-47BE-84BD-876AE6A6151D';
+
+  initialize();
   $.ajax({
     beforeSend: function (xhr) {
-      xhr.setRequestHeader('ApiKey', '49AB876F-017E-47BE-84BD-876AE6A6151D')
+      xhr.setRequestHeader('ApiKey', apiKey);
     },
     headers: {
-      'ApiKey': '49AB876F-017E-47BE-84BD-876AE6A6151D',
+      'ApiKey': apiKey,
       'Cache-Control': 'no-cache'
     },
     success: function (data) {
-      var bcycleLayer = L.layerGroup()
+      var bcycleLayer = L.layerGroup();
 
-      bcycleData = data.data
+      bcycleData = data.data;
 
       for (var i in bcycleData) {
-        var station = bcycleData[i]
-        var newStation = L.marker([station.Location.Latitude, station.Location.Longitude])
+        var station = bcycleData[i];
+        var newStation = L.marker([station.Location.Latitude, station.Location.Longitude]);
 
-        newStation.bcycleID = station.Id
-        newStation.title = station.Name
-        newStation.openBikes = station.BikesAvailable
-        newStation.openDocks = station.DocksAvailable
-        newStation.status = station.Status
+        newStation.bcycleID = station.Id;
+        newStation.title = station.Name;
+        newStation.openBikes = station.BikesAvailable;
+        newStation.openDocks = station.DocksAvailable;
+        newStation.status = station.Status;
         newStation.bindPopup('' +
           '<b>B-cycle: ' + newStation.title + '</b>' +
           '<p>Bikes available: ' + newStation.openBikes + '<br>Docks available: ' + newStation.openDocks + '</p>' +
           '<p>For more information on B-cycle, visit <a href="https://sanantonio.bcycle.com/">sanantonio.bcycle.com</a></p>.' +
-        '')
+        '');
         newStation.setIcon(L.icon({
           iconSize: [20, 20],
           iconUrl: 'icons/bcycle.gif'
-        }))
-        bcycleLayer.addLayer(newStation)
+        }));
+        bcycleLayer.addLayer(newStation);
       }
 
-      minorLayers.bcycle = bcycleLayer
+      minorLayers.bcycle = bcycleLayer;
     },
     type: 'GET',
     url: 'http://enigmatic-castle-8864.herokuapp.com/?type=json&url=' + encodeURIComponent('https://publicapi.bcycle.com/api/1.0/ListProgramKiosks/48')
-  })
+  });
   $('#buttonCloseLegend').click(function () {
-    $legend.hide()
-    $legendButton.show()
-  })
+    $legend.hide();
+    $legendButton.show();
+  });
   $legendButton.click(function () {
-    $legendButton.hide()
-    $legend.show()
-  })
+    $legendButton.hide();
+    $legend.show();
+  });
   $('#buttonTransit').click(function () {
-    var $this = $(this)
+    var $this = $(this);
 
     if ($this.text().indexOf('Hide') > -1) {
       map
         .removeLayer(transitStops)
-        .removeLayer(transitRoutes)
+        .removeLayer(transitRoutes);
       $this
         .removeClass('active')
-        .text('Show Transit')
+        .text('Show Transit');
     } else {
       map
         .addLayer(transitStops)
-        .addLayer(transitRoutes)
+        .addLayer(transitRoutes);
       $this
         .addClass('active')
-        .text('Hide Transit')
+        .text('Hide Transit');
     }
-  })
-})
+  });
+});
