1e6fbb1c42e48f83cfc0f3c89c0a11cbbc4041e3
Logan Nash <nash.logan@gmail.com>
2017-04-20 15:15:53 -0400

    Check transit.land issue

--- assets/js/main.js
+++ assets/js/main.js
@@ -511,7 +511,7 @@ NPMap = {
 			  },
 			  type: 'GET',
 			  // Use the route's onestop ID to retrieve all stops associated with this route
-			  url: 'https://transit.land/api/v1/routes.geojson?operated_by=o-9v1z-viametropolitantransit&imported_with_gtfs_id=true&gtfs_id='+String(transitRouteNumbers[i])
+			  url: 'https://transit.land/api/v1/routes.geojson?operated_by=o-9v1z-viametropolitantransit&gtfs_id='+String(transitRouteNumbers[i])
 			});
         }
 
